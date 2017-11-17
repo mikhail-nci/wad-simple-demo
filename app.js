16c0c09c776fbfe3851dc8846085551095e4ae9a
mtimofeev <mtimofeev@ncirl.ie>
2017-11-17 09:33:48 -0500

    added new XML-JSON conversion

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var http = require('http'),
-    path = require('path'),
-    express = require('express'),
-    bodyParser = require('body-parser'),
-    fs = require('fs'),
-    js2xmlparser = require('js2xmlparser'),
-    libxslt = require('libxslt');
+  path = require('path'),
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  fs = require('fs'),
+  xml2js = require('xml2js'),
+  libxslt = require('libxslt');
 
 var router = express();
 var server = http.createServer(router);
 
 router.use(express.static(path.resolve(__dirname, 'views')));
-router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
 router.use(bodyParser.json());
 
 // GET request to dislay index.html located inside /views folder
@@ -20,53 +22,59 @@ router.get('/', function(req, res) {
 
 // HTML produced by XSL Transformation
 router.get('/get/html', function(req, res) {
-  
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    
-    var docSource = fs.readFileSync('Countries.xml', 'utf8');
-    var stylesheetSource = fs.readFileSync('Countries.xsl', 'utf8');
-    
-    var doc = libxslt.libxmljs.parseXml(docSource);
-    var stylesheet = libxslt.parse(stylesheetSource);
-    
-    var result = stylesheet.apply(doc);
-    
-    res.end(result.toString());
-  
+
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+
+  var docSource = fs.readFileSync('Countries.xml', 'utf8');
+  var stylesheetSource = fs.readFileSync('Countries.xsl', 'utf8');
+
+  var doc = libxslt.libxmljs.parseXml(docSource);
+  var stylesheet = libxslt.parse(stylesheetSource);
+
+  var result = stylesheet.apply(doc);
+
+  res.end(result.toString());
+
 });
 
 // POST request to add to JSON & XML files
 router.post('/post/json', function(req, res) {
 
+  // Function to read in XML file and convert it to JSON
+  function xmlFileToJs(filename, cb) {
+    var filepath = path.normalize(path.join(__dirname, filename));
+    fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+      if (err) throw (err);
+      xml2js.parseString(xmlStr, {}, cb);
+    });
+  }
+
+  //Function to convert JSON to XML and save it
+  function jsToXmlFile(filename, obj, cb) {
+    var filepath = path.normalize(path.join(__dirname, filename));
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(obj);
+    fs.writeFile(filepath, xml, cb);
+  }
+
   // Function to read in a JSON file, add to it & convert to XML
   function appendJSON(obj) {
 
-    // Read in a JSON file
-    var JSONfile = fs.readFileSync('Countries.json', 'utf8');
-
-    // Parse the JSON file in order to be able to edit it 
-    var JSONparsed = JSON.parse(JSONfile);
-
-    // Add a new record into country array within the JSON file    
-    JSONparsed.country.push(obj);
-
-    // Beautify the resulting JSON file
-    var JSONformated = JSON.stringify(JSONparsed, null, 4);
-
-    // Write the updated JSON file back to the system 
-    fs.writeFileSync('Countries.json', JSONformated);
-
-    // Convert the updated JSON file to XML     
-    var XMLformated = js2xmlparser.parse("countries", JSON.parse(JSONformated));
-
-    // Write the resulting XML back to the system
-    fs.writeFileSync('Countries.xml', XMLformated);
-
+    // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
+    xmlFileToJs('Countries.xml', function(err, result) {
+      if (err) throw (err);
+      result.countries.country.push(obj);
+      jsToXmlFile('Countries.xml', result, function(err) {
+        if (err) console.log(err);
+      })
+    })
   }
 
   // Call appendJSON function and pass in body of the current POST request
   appendJSON(req.body);
-  
+
   // Re-direct the browser back to the page, where the POST request came from
   res.redirect('back');
 
