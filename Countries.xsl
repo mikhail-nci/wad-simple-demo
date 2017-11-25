a13186726a702ada66386a6344c2346975cd71da
mtimofeev <mtimofeev@ncirl.ie>
2017-11-25 10:50:19 -0500

    Added all modules and new functionality

--- Countries.xsl
+++ Countries.xsl
@@ -1,24 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
 	<xsl:template match="/">
-		<html>
-			<head>
-				<style>
-				  table {
-				    border-collapse: collapse;
-				  }
-				  td, th {
-				    border: 1px solid #999;
-				    padding: 0.5rem;
-				    text-align: left;
-				  }
-				  th {
-				    font-weight: bold;
-				  }
-			  </style>
-			</head>
-			<body>
-				<table>
+				<table id="table">
 					<tr>
 						<th>Country code</th>
 						<th>Name</th>
@@ -26,7 +9,7 @@
 						<th>Phone code</th>
 					</tr>
 					<xsl:for-each select="countries/country">
-							<tr>
+							<tr id="{position()}">
 								<td>
 									<xsl:value-of select="Country_code"/>
 								</td>
@@ -42,7 +25,5 @@
 							</tr>
 					</xsl:for-each>
 				</table>
-			</body>
-		</html>
 	</xsl:template>
 </xsl:stylesheet>
--- app.js
+++ app.js
@@ -80,6 +80,48 @@ router.post('/post/json', function(req, res) {
 
 });
 
+// POST request to add to JSON & XML files
+router.post('/post/delete', function(req, res) {
+
+  // Function to read in XML file and convert it to JSON
+  function xmlFileToJs(filename, cb) {
+    var filepath = path.normalize(path.join(__dirname, filename));
+    fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+      if (err) throw (err);
+      xml2js.parseString(xmlStr, {}, cb);
+    });
+  }
+
+  //Function to convert JSON to XML and save it
+  function jsToXmlFile(filename, obj, cb) {
+    var filepath = path.normalize(path.join(__dirname, filename));
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(obj);
+    fs.writeFile(filepath, xml, cb);
+  }
+
+  // Function to read in a JSON file, add to it & convert to XML
+  function deleteJSON(obj) {
+
+    console.log(obj);
+//     // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
+//     xmlFileToJs('Countries.xml', function(err, result) {
+//       if (err) throw (err);
+//       result.countries.country.push(obj);
+//       jsToXmlFile('Countries.xml', result, function(err) {
+//         if (err) console.log(err);
+//       })
+//     })
+  }
+
+  // Call appendJSON function and pass in body of the current POST request
+  deleteJSON(req.body);
+
+  // Re-direct the browser back to the page, where the POST request came from
+  res.redirect('back');
+
+});
+
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
   var addr = server.address();
   console.log("Server listening at", addr.address + ":" + addr.port);
