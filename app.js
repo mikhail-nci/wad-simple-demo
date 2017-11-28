3225285febbb184e615a62e2ab355ab28a9d743f
mtimofeev <mtimofeev@ncirl.ie>
2017-11-28 09:52:56 -0500

    added Delete button

--- app.js
+++ app.js
@@ -15,6 +15,23 @@ router.use(bodyParser.urlencoded({
 }));
 router.use(bodyParser.json());
 
+// Function to read in XML file and convert it to JSON
+function xmlFileToJs(filename, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+    if (err) throw (err);
+    xml2js.parseString(xmlStr, {}, cb);
+  });
+}
+
+//Function to convert JSON to XML and save it
+function jsToXmlFile(filename, obj, cb) {
+  var filepath = path.normalize(path.join(__dirname, filename));
+  var builder = new xml2js.Builder();
+  var xml = builder.buildObject(obj);
+  fs.writeFile(filepath, xml, cb);
+}
+
 // GET request to dislay index.html located inside /views folder
 router.get('/', function(req, res) {
   res.render('index');
@@ -42,26 +59,9 @@ router.get('/get/html', function(req, res) {
 // POST request to add to JSON & XML files
 router.post('/post/json', function(req, res) {
 
-  // Function to read in XML file and convert it to JSON
-  function xmlFileToJs(filename, cb) {
-    var filepath = path.normalize(path.join(__dirname, filename));
-    fs.readFile(filepath, 'utf8', function(err, xmlStr) {
-      if (err) throw (err);
-      xml2js.parseString(xmlStr, {}, cb);
-    });
-  }
-
-  //Function to convert JSON to XML and save it
-  function jsToXmlFile(filename, obj, cb) {
-    var filepath = path.normalize(path.join(__dirname, filename));
-    var builder = new xml2js.Builder();
-    var xml = builder.buildObject(obj);
-    fs.writeFile(filepath, xml, cb);
-  }
-
   // Function to read in a JSON file, add to it & convert to XML
   function appendJSON(obj) {
-
+     console.log(obj);
     // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
     xmlFileToJs('Countries.xml', function(err, result) {
       if (err) throw (err);
@@ -83,42 +83,20 @@ router.post('/post/json', function(req, res) {
 // POST request to add to JSON & XML files
 router.post('/post/delete', function(req, res) {
 
-  // Function to read in XML file and convert it to JSON
-  function xmlFileToJs(filename, cb) {
-    var filepath = path.normalize(path.join(__dirname, filename));
-    fs.readFile(filepath, 'utf8', function(err, xmlStr) {
-      if (err) throw (err);
-      xml2js.parseString(xmlStr, {}, cb);
-    });
-  }
-
-  //Function to convert JSON to XML and save it
-  function jsToXmlFile(filename, obj, cb) {
-    var filepath = path.normalize(path.join(__dirname, filename));
-    var builder = new xml2js.Builder();
-    var xml = builder.buildObject(obj);
-    fs.writeFile(filepath, xml, cb);
-  }
-
   // Function to read in a JSON file, add to it & convert to XML
   function deleteJSON(obj) {
-
-    console.log(obj);
-//     // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
-//     xmlFileToJs('Countries.xml', function(err, result) {
-//       if (err) throw (err);
-//       result.countries.country.push(obj);
-//       jsToXmlFile('Countries.xml', result, function(err) {
-//         if (err) console.log(err);
-//       })
-//     })
+    // Function to read in XML file, convert it to JSON, add a new object and write back to XML file
+    xmlFileToJs('Countries.xml', function(err, result) {
+      if (err) throw (err);
+      console.log(obj.row);
+      jsToXmlFile('Countries.xml', result, function(err) {
+        if (err) console.log(err);
+      })
+    })
   }
 
   // Call appendJSON function and pass in body of the current POST request
   deleteJSON(req.body);
-
-  // Re-direct the browser back to the page, where the POST request came from
-  res.redirect('back');
 
 });
 
